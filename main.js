e831e44287b6fe10685a9da5b9309e206631a1c0
izaahh <131893386+izaahh@users.noreply.github.com>
2023-11-01 12:23:12 -0300

    pp

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ function tocaSomAplausos(){
     document.querySelector ('#som_tecla_aplausos').play();
 }
 document.querySelector('tecla_aplausos').onclik=tocaSomAplausos;
+
+function tocaSomAplausos(){
+    document.querySelector ('#som_tecla_vaia').play();
+}
+document.querySelector('tecla_vaia').onclik=tocaSomAplausos;
+
+function tocaSomAplausos(){
+    document.querySelector ('#som_tecla_risada').play();
+}
+document.querySelector('tecla_risada').onclik=tocaSomAplausos;
+
+function tocaSomAplausos(){
+    document.querySelector ('#som_tecla_errado').play();
+}
+document.querySelector('tecla_errado').onclik=tocaSomAplausos;
+
+function tocaSomAplausos(){
+    document.querySelector ('#som_tecla_grilo').play();
+}
+document.querySelector('tecla_grilo').onclik=tocaSomAplausos;
