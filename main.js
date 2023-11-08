2e7f4ff5f4e870f361a75231dd4a4688303029cc
izaahh <131893386+izaahh@users.noreply.github.com>
2023-11-08 12:06:10 -0300

    cccc

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 function tocaSomAplausos(){
     document.querySelector ('#som_tecla_aplausos').play();
 }
-document.querySelector('tecla_aplausos').onclik=tocaSomAplausos;
+document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
 
 function tocaSomVaia(){
     document.querySelector ('#som_tecla_vaia').play();
 }
-document.querySelector('tecla_vaia').onclik=tocaSomVaia;
+document.querySelector('tecla_vaia').onclick=tocaSomVaia;
 
 function tocaSomlRisadas(){
     document.querySelector ('#som_tecla_risada').play();
 }
-document.querySelector('tecla_risada').onclik=tocaSomlRisadas;
+document.querySelector('tecla_risada').onclick=tocaSomlRisadas;
 
 function tocaSomErrado(){
     document.querySelector ('#som_tecla_errado').play();
 }
-document.querySelector('tecla_errado').onclik=tocaSomErrado;
+document.querySelector('tecla_errado').onclick=tocaSomErrado;
 
 function tocaSomGrilo(){
     document.querySelector ('#som_tecla_grilo').play();
 }
-document.querySelector('tecla_grilo').onclik=tocaSomGrilo;
+document.querySelector('tecla_grilo').onclick=tocaSomGrilo;
 
 function tocaSomTambores(){
     document.querySelector ('#som_tecla_tambores').play();
 }
-document.querySelector('tecla_tambores').onclik=tocaSomTambores;
+document.querySelector('tecla_tambores').onclick=tocaSomTambores;
 
 function tocaSomTrombeta(){
     document.querySelector ('#som_tecla_trombeta').play();
 }
-document.querySelector('tecla_trombeta').onclik=tocaSomTrombeta;
+document.querySelector('tecla_trombeta').onclick=tocaSomTrombeta;
 
 function tocaSomDinheiro(){
     document.querySelector ('#som_tecla_dinheiro').play();
 }
-document.querySelector('tecla_dinheiro').onclik=tocaSomDinheiro;
+document.querySelector('tecla_dinheiro').onclick=tocaSomDinheiro;
 
 function tocaSomVitoria(){
     document.querySelector ('#som_tecla_vitoria').play();
 }
-document.querySelector('tecla_vitoria').onclik=tocaSomVitoria;
+document.querySelector('tecla_vitoria').onclick=tocaSomVitoria;
 
 
 
